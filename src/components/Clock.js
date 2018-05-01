7ccb1dc8790720d24c089e624226f9f959c3d1ec
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-05-01 22:53:21 +0100

    Remove dialStyle

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import Digital from "./Digital.js";
-import { app as appStyle, dial as dialStyle } from "../styles/styles.js";
+import { app as appStyle } from "../styles/styles.js";
 
 const Clock = ( props ) => (
     <div style={ appStyle }>
-        <div style={ dialStyle }></div>
         <Digital />>
     </div>
 );
