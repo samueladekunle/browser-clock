cdb60b604bb3f01a3ee5860339bf982303534662
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-05-01 23:20:03 +0100

    Add Analog.js

---
+++ src/components/Analog.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import CurrentTime from "../CurrentTime";
+import { dial as dialStyle, secondHand as secondHandStyle, minuteHand as minuteHandStyle, hourHand as hourHandStyle } from "../styles/styles";
+
+class Analog extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            currentTime: new Date( CurrentTime.currentTime )
+        };
+        const currentTime = this.state.currentTime;
+        secondHandStyle.transform = this.toDegree( currentTime );
+        minuteHandStyle.transform = this.toDegree( currentTime );
+        hourHandStyle.transform = this.toDegree( currentTime, 12 );
+        this.updateTime();
+    }
+    get toDegree( value, divisor = 60 ) {
+        return ( value / divisor ) * 360 - 90;
+    }
+    updateTime() {
+        setInterval( () => this.setState( { currentTime: CurrentTime.currentTime } ), 1000 );
+    }
+    render() {
+        return (
+            <div>
+                <div style={ dialStyle }></div>
+                <div style={ secondHandStyle }></div>
+                <div style={ minuteHandStyle }></div>
+                <div style={ hourHandStyle }></div>
+            </div>
+        );
+    }
+}
