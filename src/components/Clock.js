3de3770b1baf6861e0d8fda4a71bf327e32cd0fc
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-05-01 21:08:14 +0100

    Add Clock.js

---
+++ src/components/Clock.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { app as appStyle, dial as dialStyle } from "../styles/styles.js";
+
+const Clock = ( props ) => (
+    <div style={ appStyle }>
+        <div style={ dialStyle }></div>
+    </div>
+);
