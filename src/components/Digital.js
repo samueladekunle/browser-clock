5362e3858ca11373a8531bbcc8dde102aa5a30bb
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-05-01 22:00:56 +0100

    Add Digital.js

---
+++ src/components/Digital.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import CurrentTime from "../CurrenTime.js";
+
+class Digital extends Component {
+    constructor( props ) {
+        super( props );
+        this.updateTime = this.updateTime.bind(this);
+        this.state = {
+            currentTime: CurrentTime
+        };
+        this.updateTime();
+    }
+    updateTime() {
+        setInterval( () => this.setState( { currentTime: CurrentTime } ), 1000 );
+    }
+    render() {
+        return <div>{ this.state.currentTime }</div>;
+    }
+}
