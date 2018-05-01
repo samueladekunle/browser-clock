c9571e991b78c3880f6645df2f0f1dd555d50a67
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-05-01 22:06:08 +0100

    Import Digital component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Digital from "./Digital.js";
 import { app as appStyle, dial as dialStyle } from "../styles/styles.js";
 
 const Clock = ( props ) => (
     <div style={ appStyle }>
         <div style={ dialStyle }></div>
+        <Digital />>
     </div>
 );
 
