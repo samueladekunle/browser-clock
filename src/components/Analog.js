07f6f947f27e7042acee79f4153e4f20b7643e79
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-05-02 07:52:47 +0100

    Add transform property to style rules

--- src/components/Analog.js
+++ src/components/Analog.js
@@ -6,12 +6,8 @@ class Analog extends Component {
     constructor( props ) {
         super( props );
         this.state = {
-            currentTime: new Date( CurrentTime.currentTime )
+            currentTime: CurrentTime.currentTime
         };
-        const currentTime = this.state.currentTime;
-        secondHandStyle.transform = this.toDegree( currentTime );
-        minuteHandStyle.transform = this.toDegree( currentTime );
-        hourHandStyle.transform = this.toDegree( currentTime, 12 );
         this.updateTime();
     }
     toDegree( value, divisor = 60 ) {
@@ -20,12 +16,30 @@ class Analog extends Component {
     updateTime() {
         setInterval( () => this.setState( { currentTime: CurrentTime.currentTime } ), 1000 );
     }
+    transformStyle( style, transform ) {
+        return Object.assign( {}, style, transform );
+    }
+    transform( degree ) {
+        return { transform: `rotate(${ degree }deg)` };
+    }
     render() {
+        const currentTime = new Date ( this.state.currentTime );
+        const second = currentTime.getSeconds();
+        const minute = currentTime.getMinutes();
+        const hour = currentTime.getHours();
+        
+        const secondInDegree = this.toDegree( second ).toString();
+        const minuteInDegree = this.toDegree( minute ).toString();
+        const hourInDegree = this.toDegree( hour, 12 ).toString();
+
+        this.secondHandStyle = this.transformStyle( secondHandStyle, this.transform( secondInDegree ) );
+        this.minuteHandStyle = this.transformStyle( minuteHandStyle, this.transform( minuteInDegree ) );
+        this.hourHandStyle = this.transformStyle( hourHandStyle, this.transform( hourInDegree ) );
         return (
             <div style={ dialStyle }>
-                <div style={ secondHandStyle }></div>
-                <div style={ minuteHandStyle }></div>
-                <div style={ hourHandStyle }></div>
+                <div style={ this.secondHandStyle }></div>
+                <div style={ this.minuteHandStyle }></div>
+                <div style={ this.hourHandStyle }></div>
             </div>
         );
     }
